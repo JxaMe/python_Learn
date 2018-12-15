6e2c3265f5942fbe22d12323adb9b5cadd16968f
loste <loster0219@gmail.com>
2018-12-15 20:40:53 +0800

    <Python Crash Course> exercises <Python从入门到实践> 练习题

--- Chapter 7/7-6.py
+++ Chapter 7/7-6.py
@@ -7,3 +7,44 @@
 - 使用变量 active 来控制循环结束的时机。
 使用 break 语句在用户输入'quit'时退出循环
 '''
+
+'''
+while True:
+    msg = input("请输入你的年龄:")
+    if msg == 'quit':
+        break
+    else:
+        msg = int(msg)
+        if msg < 3:
+            print("免费")
+        elif msg >= 3 and msg <= 12:
+            print('收费10美元')
+        else:
+            print("收费15美元")
+'''
+'''
+active = True
+while active:
+    msg = input("请输入你的年龄:")
+    if msg == 'quit':
+        active = False
+    else:
+        msg = int(msg)
+        if msg < 3:
+            print("免费")
+        elif msg >= 3 and msg <= 12:
+            print('收费10美元')
+        else:
+            print("收费15美元")
+'''
+
+msg = ''
+while msg != 'quit':
+    msg = input("请输入你的年龄:")
+    msg = int(msg)
+    if msg < 3:
+        print("免费")
+    elif msg >= 3 and msg <= 12:
+        print('收费10美元')
+    else:
+        print("收费15美元")
