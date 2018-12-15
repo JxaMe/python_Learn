7e21afa5f0a47928f0b18d007e961b7cc7436778
loste <loster0219@gmail.com>
2018-12-15 21:05:06 +0800

    <Python Crash Course> exercises <Python从入门到实践> 练习题

--- Chapter 7/7-8.py
+++ Chapter 7/7-8.py
@@ -4,6 +4,7 @@
 '''
 熟食店：创建一个名为 sandwich_orders 的列表，在其中包含各种三明治的名字；
 再创建一个名为 finished_sandwiches 的空列表。
-遍历列表 sandwich_orders，对于其中的每种三明治，都打印一条消息，如 I made your tuna sandwich，并将其移到列表
-finished_sandwiches。所有三明治都制作好后，打印一条消息，将这些三明治列出来
+遍历列表 sandwich_orders，对于其中的每种三明治，都打印一条消息，如 I made your tuna sandwich
+并将其移到列表finished_sandwiches。
+所有三明治都制作好后，打印一条消息，将这些三明治列出来
 '''
