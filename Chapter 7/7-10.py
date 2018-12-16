a26df517c51359898b04a339e0f8f685639e5702
loste <loster0219@gmail.com>
2018-12-16 11:15:19 +0800

    <Python Crash Course> exercises <Python从入门到实践> 练习题

--- Chapter 7/7-10.py
+++ Chapter 7/7-10.py
@@ -5,3 +5,18 @@
 梦想的度假胜地：编写一个程序，调查用户梦想的度假胜地。
 使用类似于“If you could visit one place in the world, where would you go?”的提示，并编写一个打印调查结果的代码块。
 '''
+
+info = {}
+while True:
+    name = input("请输入你的姓名:")
+    msg = input("你最想去哪里度假:")
+
+    info[name] = msg
+    print(info)
+    repeat = input("你还想继续调查吗?yes/no")
+
+    if repeat == 'no':
+        break
+
+for name, msg in info.items():
+    print('姓名:'+name+' '+'度假地:'+msg)
--- Chapter 7/7-8.py
+++ Chapter 7/7-8.py
@@ -8,3 +8,14 @@
 并将其移到列表finished_sandwiches。
 所有三明治都制作好后，打印一条消息，将这些三明治列出来
 '''
+
+sandwich_orders = ['onesandwich', 'twosandwich', 'threesandwich']
+
+finished_sandwiches = []
+for sandwich in sandwich_orders:
+    print('I made your ' + sandwich)
+while sandwich_orders:
+    temp = sandwich_orders.pop()
+    finished_sandwiches.append(temp)
+
+print(finished_sandwiches)
